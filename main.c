066e39aef3c2f3f8ea834f9db6b50f766bddd0ec
Long Tran <ltran0325@gmail.com>
2021-02-26 17:13:45 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
-*                       MSP432 Keypad 7-Segment Solution                       *                  
-*                                                                              *                 
-* Author:  Long Tran                                                           *                                                       
+*                       MSP432 Keypad 7-Segment Solution                       *
+*                                                                              *
+* Author:  Long Tran                                                           *
 * Device:  MSP432P401R LaunchPad                                               *
 * Program: Display input to keypad                                             *
 *                                                                              *
@@ -15,11 +15,11 @@
 
 // Include header file(s) and define constants
 #include "msp.h"
-#define N 300       // debounce loop count
+#define N 100       // debounce loop count
 #define d 16        // digit-bit index to clear display
 
 // Define digit-bit lookup table
-const int digit_array[17] = { 
+const int digit_array[17] = {
 0b11000000,  // 0
 0b11111001,  // 1
 0b10100100,  // 2
@@ -40,14 +40,14 @@ const int digit_array[17] = {
 };
 
 // Define keypad layout
-const int keypad_table[4][9] = {        
+const int keypad_table[4][9] = {
     {d, 10,  3, d, 2, d, d, d,  1},     // 4x9 multiplixer keypad
     {d, 11,  6, d, 5, d, d, d,  4},     // only columns 1, 2, 4, 8 are valid for single keypres
     {d, 12,  9, d, 8, d, d, d,  7},
     {d, 13, 15, d, 0, d, d, d, 14}};
 
 // Define keypad states of FSM
-typedef enum{      
+typedef enum{
     IDLE,           // scan for keypad input
     PRESS,          // debounce keypad press
     PROCESS,        // accept input key
@@ -55,7 +55,7 @@ typedef enum{
 }KeyStates;
 
 // Define keypad structure to handle related variables
-typedef struct{         // KEYPAD STRUCTURE 
+typedef struct{         // KEYPAD STRUCTURE
     KeyStates state;    // states of keypad FSM
     int x;              // x position of pressed key
     int y;              // y position of pressed key
@@ -70,7 +70,7 @@ void gpio_init(void);   // initialize GPIO
 void wait(int t);       // busy wait
 
 void main(void)
-{   
+{
     // Initialize GPIO and keypad structure
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;                // disable watchdog
     gpio_init(); int temp;                                     // initalize GPIO
@@ -81,77 +81,79 @@ void main(void)
         P4->OUT = 0xFF;                     // blank display
         P8->OUT = 0xFF & ~(BIT5 >> key.k);  // enable k-th display
         P4->OUT = key.display[key.k];       // display k-th char in array
-        
+
         // Scan for keypad input in row-k
         temp = (P9->IN) & 0x0F;             // scan input at row-k
         if(temp > 0 ){                      // if key pressed detected
             key.x = temp;                   // acknowledge input x position
             key.y = key.k;                  // acknowledge input y position
         }
-        
+
         // Increment index-k
-        key.k++;                            
+        key.k++;
         if (key.k >= 4){key.k = 0;}
-        
+
         // Test program resistance to software updates (observe LED lighting)
-        wait(100);                          
-                        
+        wait(100);
+
         // Switch keypad debouncing state
         switch (key.state){
-            
+
             // Wait for input
-            case IDLE:                              
+            case IDLE:
                 if(key.x != 0){
-                    key.state = PRESS;              
+                    key.state = PRESS;
                     key.pulses = 0;
                 }break;
-            
+
             // Accept input if N pulses of HIGH detected
-            case PRESS:                             
+            case PRESS:
                 P4->OUT = 0xFF;                     // blank display
                 P8->OUT = 0xFF & ~(BIT5 >> key.y);  // switch to row where input was prev found
                 temp = (P9->IN) & 0x0F;             // read column input
                 if(temp == key.x){key.pulses++;}    // increment pulses if last input is same as new input
+                else{
+                    key.pulses = 0;
+                    key.state = IDLE;            // input success
+                }
                 if(key.pulses > N){
                     key.pulses = 0;
                     key.state = PROCESS;            // input success
-                }else{
-                    key.state = IDLE;               
                 }break;
-            
+
             // Update display array with accepted input
-            case PROCESS:                           
+            case PROCESS:
                 if(key.display_count > 3){key.display_count = 0;}
                 key.display[key.display_count] = digit_array[keypad_table[key.y][key.x]];
                 key.display_count++;
-                key.state = RELEASE;                    
+                key.state = RELEASE;
                 break;
-    
+
             // Accept release if N pulses of LOW detected
-            case RELEASE:                               
+            case RELEASE:
                 P4->OUT = 0xFF;                          // blank display
                 P8->OUT = 0xFF & ~(BIT5 >> key.y);       // switch to row where input was prev found
                 temp = (P9->IN) & 0x0F;                  // read keypad column input
                 if(temp == 0){key.pulses++;}
+                else{
+                    key.pulses = 0;
+                    key.state = RELEASE;
+                }
                 if(key.pulses > N){
                     key.pulses = 0;
                     key.state = IDLE;                    // release successful
-                }else{
-                    key.pulses = 0;
-                    key.state = RELEASE;    
                 }break;
 
         }// switch end
     }// while(1) end
 }// main end
 
-void gpio_init(void){        
+void gpio_init(void){
     P4->DIR = 0xFF;          // P4 is LED output
     P8->DIR = 0xFF;          // P8 is display output
     P9->DIR = 0x00;          // P9 is keypad input
 }
-    
-void wait(int t){           
+
+void wait(int t){
     while(t >= 0){t--;}
 }
-
